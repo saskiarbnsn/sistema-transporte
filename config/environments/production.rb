1a2b5b70e058d1d7ac4ef075b05753d8a96f6f8f
Saskia Robinson <saskiarbnsn54@gmail.com>
2023-02-08 18:04:07 -0300

    set up devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,8 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
+  config.i18n.default_locale = :es
+  config.i18n.available_locales = [:es,:en]
   config.i18n.fallbacks = true
 
   # Don't log any deprecations.
