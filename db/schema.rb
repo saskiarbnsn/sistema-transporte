14efe6f565e89c0cd9454d5a83196aa91268510f
Saskia Robinson <saskiarbnsn54@gmail.com>
2023-02-08 15:35:32 -0300

    add unique index customer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_08_171817) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_08_183150) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_08_171817) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["cuit"], name: "index_customers_on_cuit", unique: true
   end
 
   create_table "drivers", force: :cascade do |t|
