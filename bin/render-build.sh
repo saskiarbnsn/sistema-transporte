129d90a8b810ca5e7cb00de78479c5a36a741e7e
saskiarbnsn <saskiarbnsn54@gmail.com>
2026-06-04 00:29:48 -0300

    Restore normal migration step in build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,4 @@ bundle install
 yarn install --frozen-lockfile
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails runner "ActiveRecord::Base.connection.execute('DROP SCHEMA public CASCADE; CREATE SCHEMA public;')"
-bundle exec rails db:schema:load
-bundle exec rails db:seed
+bundle exec rails db:migrate
