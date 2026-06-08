1f9e00ae00029463a154c457337c0fb86d07c638
saskiarbnsn <saskiarbnsn54@gmail.com>
2026-06-08 05:32:29 -0300

    seed1

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,5 @@ bundle install
 yarn install --frozen-lockfile
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
-bundle exec rails db:migrate
+bundle exec rails db:schema:load
 bundle exec rails db:seed
